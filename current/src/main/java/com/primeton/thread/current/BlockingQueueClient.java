c59b93c49658fc7db109d47c20a6fc3d87b30583
liugg <liugg@primeton.com>
2017-07-08 07:44:15 +0800

    up

---
+++ current/src/main/java/com/primeton/thread/current/BlockingQueueClient.java
@@ -0,0 +1,25 @@
+package com.primeton.thread.current;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class BlockingQueueClient {
+
+	public static void main(String[] args) {
+		
+		BlockingQueue queue = new ArrayBlockingQueue(1024);  
+		  
+        Producer producer = new Producer(queue);  
+        Consumer consumer = new Consumer(queue);  
+  
+        new Thread(producer).start();  
+        new Thread(consumer).start();  
+  
+        try {
+			Thread.sleep(4000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}  
+	}
+
+}
